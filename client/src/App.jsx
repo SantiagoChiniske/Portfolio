112dd715e4ddc274ef7ca2dc74bab11e90739691
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2024-11-07 20:53:09 -0300

    ajustes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useScroll} from "framer-motion";
 import Header  from "./components/header/header"
 import Start from "./components/start/start"
 import About from "./components/about/about"
@@ -5,18 +6,21 @@ import Tecnologias from "./components/tecnologias/tecnologias"
 import Projetos from "./components/projetos/projetos"
 import Contato from "./components/contato/contato"
 import Footer from "./components/footer/footer"
+import { useRef } from "react";
+
+
 
 function App() {
 
   return (
     <>
-      <Header/>
-      <Start/>
-      <About/>
-      <Tecnologias/>
-      <Projetos/>
-      <Contato/>
-      <Footer/>
+    <Header/>
+    <Start/>
+    <About/>
+    <Tecnologias/>
+    <Projetos/>
+    <Contato/>
+    <Footer/>
     </>
   )
 }
